6fe31a022df7c18faeb5701a3e82a568faa0c170
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-05 00:29:02 +0430

    favicon & meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Memo Game",
   description: "Memo Game Application",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  themeColor: '#f8f8f8',
+  keywords: ['game', 'memo'],
+  appleWebApp: {
+    title: "Memo Game",
+    capable: true,
+    statusBarStyle: 'default',
+  },
+  other: {
+    "msapplication-TileColor": "#f8f8f8",
+    "msapplication-TileImage": "/logo.png",
+    "theme-color": "#f8f8f8",
+    "apple-mobile-web-app-status-bar-style": "default",
+    "apple-mobile-web-app-title": "Memo Game",
+    "google": "notranslate",
+  }
+
 };
 
 export default function RootLayout({
